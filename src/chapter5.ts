4175752e3e9620b389f139a2d9f6fc1fd61e1c5c
yuizho <yuizho3@gmail.com>
2020-09-28 00:51:34 +0900

    add factory pattern sample to chapter5.ts

--- src/chapter5.ts
+++ src/chapter5.ts
@@ -22,3 +22,36 @@ giveFood(new Dog(), "apple");
 // Cat can set as parameter.because Cat class has same method
 // but if Dog has private field, Cat can not set here.
 giveFood(new Cat(), "apple");
+
+// -------- design pattern
+// factory
+type Shoe = {
+  purpose: string;
+};
+
+class BalletFlat implements Shoe {
+  purpose = "dancing";
+}
+
+class Boot implements Shoe {
+  purpose = "woodcutting";
+}
+
+class Sneaker implements Shoe {
+  purpose = "walking";
+}
+
+let Shoe = {
+  create(type: "balletFlat" | "boot" | "sneaker"): Shoe {
+    switch (type) {
+      case "balletFlat":
+        return new BalletFlat();
+      case "boot":
+        return new Boot();
+      case "sneaker":
+        return new Sneaker();
+    }
+  },
+};
+
+console.log(Shoe.create("boot").purpose);
